586fe9e7948f283741b2749ec760cc956cc79944
weizongshang <619544688@qq.com>
2017-09-01 10:56:57 +0800

    format number

--- javascript/parseJson.js
+++ javascript/parseJson.js
@@ -4,6 +4,12 @@ let dataObj = JSON.parse(data);
 
 var stream = fs.createWriteStream(process.argv[3]);
 stream.once('open', function(fd) {
-  dataObj.out.gpsItemEntities.forEach((item) => stream.write( Object.keys(item).map(key => item[key])+"\n" ) );
+  dataObj.out.source.gpsItemEntities.forEach( (item) => 
+      stream.write( 
+          Object.keys(item).map( 
+              key=> " "+(item[key] > 1460000000?item[key]:item[key].toFixed(7)) 
+          )+"\n" 
+      )
+  )
   stream.end();
 });

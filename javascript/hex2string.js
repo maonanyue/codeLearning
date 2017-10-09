ff49a370a39ce12b4fcce267f70ecef32582069c
weizongshang <619544688@qq.com>
2017-10-09 16:06:01 +0800

    convert hex array string to string

---
+++ javascript/hex2string.js
@@ -0,0 +1,32 @@
+var readline = require('readline'),
+    fs = require('fs');
+
+
+function hex2string( data ){
+    let b = data.split(',')
+    let buffer = new Buffer(b.length)
+    let i = 0
+    b.forEach( function(v){ buffer.writeUInt8( Number.parseInt(v, 16), i++ ) })
+    console.log(buffer.toString('utf-8'))
+}
+
+
+let data = process.argv[2]
+
+if(process.argv[2] !== "-f"){
+    hex2string(data)
+    return
+}
+
+
+//    data = fs.readFileSync(process.argv[3]);
+
+let rl = readline.createInterface({
+    input: fs.createReadStream(process.argv[3]),
+    output: process.stdout,
+    terminal: false
+});
+
+rl.on('line', function(line) {
+    hex2string(line)
+});

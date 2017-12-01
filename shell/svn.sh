b45b68d85f70b3e0b982c5b2de5c9645195cad9f
weizongshang <619544688@qq.com>
2017-12-01 11:22:17 +0800

    update all svn

--- shell/svn.sh
+++ shell/svn.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
-baseDir=$(dirname "$0")
-SVN_DIRS=`find $baseDir -maxdepth 3 -mindepth 3 -type d`
 
-svn $1 $SVN_DIRS
+function call_svn(){
+action=$1
+depth=$2
+DIRS=`find $baseDir -maxdepth ${depth} -mindepth ${depth} -type d`
+
+for curDir in $DIRS
+do
+    if [ -d "$curDir/.svn" ]; then
+        svn $action $curDir
+    fi
+done
+
+}
+
+#baseDir=$(dirname "$0")
+#SVN_DIRS=`find $baseDir -maxdepth 3 -mindepth 3 -type d`
+#echo $SVN_DIRS
+##svn $1 $SVN_DIRS
+
+for depth in 1 2 3
+do
+    call_svn $1 $depth
+done
